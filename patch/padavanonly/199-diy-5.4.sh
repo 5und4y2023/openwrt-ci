10826148fe843904e55f27492e266e8b30a3e2a8
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-11-22 19:21:35 +0800

    Update 199-diy-5.4.sh

--- patch/padavanonly/199-diy-5.4.sh
+++ patch/padavanonly/199-diy-5.4.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-uci set firewall.@defaults[0].input='ACCEPT'
-uci set firewall.@defaults[0].forward='ACCEPT'
+#uci set firewall.@defaults[0].input='ACCEPT'
+#uci set firewall.@defaults[0].forward='ACCEPT'
 uci commit firewall
-mv /diy4me/socat /etc/config/socat
-mv /diy4me/zerotier /etc/config/zerotier
+#mv /diy4me/socat /etc/config/socat
+#mv /diy4me/zerotier /etc/config/zerotier
 
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
@@ -12,13 +12,13 @@ uci delete ttyd.@ttyd[0].interface
 # 设置所有网口可连接 SSH
 uci set dropbear.@dropbear[0].Interface=''
 
-uci set wireless.default_MT7981_1_1.ssid=CMCC-RTG
-uci set wireless.default_MT7981_1_1.encryption=psk2+ccmp
-uci set wireless.default_MT7981_1_1.key=Rtg@168$
+#uci set wireless.default_MT7981_1_1.ssid=CMCC-RTG
+#uci set wireless.default_MT7981_1_1.encryption=psk2+ccmp
+#uci set wireless.default_MT7981_1_1.key=Rtg@168$
 
-uci set wireless.default_MT7981_1_2.ssid=CMCC-RTG-5G
-uci set wireless.default_MT7981_1_2.encryption=psk2+ccmp
-uci set wireless.default_MT7981_1_2.key=Rtg@168$
+#uci set wireless.default_MT7981_1_2.ssid=CMCC-RTG-5G
+#uci set wireless.default_MT7981_1_2.encryption=psk2+ccmp
+#uci set wireless.default_MT7981_1_2.key=Rtg@168$
 uci commit wireless
 
 uci commit
@@ -34,8 +34,8 @@ sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/
 #sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 #sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 
-/etc/init.d/socat restart
-/etc/init.d/zerotier restart
+#/etc/init.d/socat restart
+#/etc/init.d/zerotier restart
 /etc/init.d/network restart >/dev/null 2>&1
 /etc/init.d/firewall restart >/dev/null 2>&1
 /etc/init.d/dnsmasq restart >/dev/null 2>&1
