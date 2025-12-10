f32b492222aa98c692160416cb50b626010e7a50
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-12-11 01:34:02 +0800

    Update 199-diy.sh

--- patch/hanwckf/199-diy.sh
+++ patch/hanwckf/199-diy.sh
@@ -8,28 +8,29 @@ uci set dropbear.@dropbear[0].Interface=''
 
 uci commit
 
-# uci del network.wan6
-uci set network.lan.ip6assign=64
-uci del network.globals.ula_prefix
-uci set dhcp.lan.dns_service='0'
+uci del network.wan6
+uci del network.lan.ip6assign
+uci del dhcp.lan.ra
+uci del dhcp.lan.ra_slaac
+uci del dhcp.lan.dns_service
 uci del dhcp.lan.dhcpv6
 uci del dhcp.lan.ndp
-uci set dhcp.lan.ra='server'
 uci del dhcp.lan.ra_flags
 uci add_list dhcp.lan.ra_flags='none'
-uci set network.lan.delegate='0'
-uci set network.lan.ip6ifaceid='random'
+uci del network.globals.ula_prefix
+
+uci set network.lan.ipaddr=10.0.0.1
 
 uci commit dhcp
 uci commit network
 
-#uci set wireless.default_MT7981_1_1.ssid=xiaoguo
+uci set wireless.default_MT7981_1_1.ssid=Kwrt-2.4G
 uci set wireless.default_MT7981_1_1.encryption=psk2+ccmp
-uci set wireless.default_MT7981_1_1.key=password
+uci set wireless.default_MT7981_1_1.key=66668888
 
-#uci set wireless.default_MT7981_1_2.ssid=TK888
+uci set wireless.default_MT7981_1_2.ssid=Kwrt-5G
 uci set wireless.default_MT7981_1_2.encryption=psk2+ccmp
-uci set wireless.default_MT7981_1_2.key=password
+uci set wireless.default_MT7981_1_2.key=66668888
 uci commit wireless
 uci commit
 
@@ -37,11 +38,10 @@ sed -i '/ssrp/d' /etc/opkg/distfeeds.conf
 sed -i '/helloworld/d' /etc/opkg/distfeeds.conf
 sed -i '/passwall/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
-sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
-sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
-
-cp /etc/my-clash /etc/openclash/core/clash_meta
-
+#sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+#sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+sed -i 's/root::0:0:99999:7:::/root:$1$ZXWYOFQt$n8kWpRWwiJI5JdzQ9.POM1:0:0:99999:7:::/g' /etc/shadow
+sed -i 's/root:::0:99999:7:::/root:$1$ZXWYOFQt$n8kWpRWwiJI5JdzQ9.POM1:0:0:99999:7:::/g' /etc/shadow
 
 /etc/init.d/network restart
 
