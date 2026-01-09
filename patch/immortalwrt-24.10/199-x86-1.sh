24fecc69a496fa39de4264ddc620e6069bb77b9d
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2026-01-09 14:06:38 +0800

    Update 199-x86-1.sh

--- patch/immortalwrt-24.10/199-x86-1.sh
+++ patch/immortalwrt-24.10/199-x86-1.sh
@@ -20,8 +20,6 @@ sed -i '/targets/d' /etc/opkg/distfeeds.conf
 sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.4/targets/x86/64/kmods/6.6.110-1-f8c5d7fde74fa4fedf4370775255c515' /etc/opkg/distfeeds.conf
 sed -i '$a src/gz immortalwrt_core https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.4/targets/x86/64/packages' /etc/opkg/distfeeds.conf
 
-cp /etc/my-clash /etc/openclash/core/clash_meta
-
 
 # 计算网卡数量
 count=0
