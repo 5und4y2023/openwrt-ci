20cc01ef5b599dd8cb7f350dd9923df00038553e
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2026-03-26 13:35:18 +0800

    Create 199-mt798x.sh

---
+++ patch/openwrt-25.12/199-mt798x.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+uci -q get system.@imm_init[0] > "/dev/null" || uci -q add system imm_init > "/dev/null"
+
+if ! uci -q get system.@imm_init[0].system_chn > "/dev/null"; then
+	uci -q batch <<-EOF
+		set system.@system[0].timezone="CST-8"
+		set system.@system[0].zonename="Asia/Shanghai"
+
+		delete system.ntp.server
+		add_list system.ntp.server="ntp.tencent.com"
+		add_list system.ntp.server="ntp1.aliyun.com"
+		add_list system.ntp.server="ntp.ntsc.ac.cn"
+		add_list system.ntp.server="cn.ntp.org.cn"
+
+		set system.@imm_init[0].system_chn="1"
+		commit system
+	EOF
+fi
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+
+#其他网络设置
+uci commit
+
+sed -i '/modem/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/passwall/d' /etc/apk/repositories.d/distfeeds.list
+sed -i 's#downloads.openwrt.org#mirrors.pku.edu.cn/openwrt#g' /etc/apk/repositories.d/distfeeds.list
+sed -i '/filogic/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://mirrors.pku.edu.cn/openwrt/releases/25.12.1/targets/mediatek/filogic/kmods/6.12.74-1-60d938adcb727697d3015e4285d4c290/packages.adb' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://mirrors.pku.edu.cn/openwrt/releases/25.12.1/targets/mediatek/filogic/packages/packages.adb' /etc/apk/repositories.d/distfeeds.list
+
+#sed -i 's/https/http/g' /etc/opkg/distfeeds.conf
+
+
+# wifi设置
+uci set wireless.default_radio0.ssid=OpenWrt-2.4G
+uci set wireless.default_radio1.ssid=OpenWrt-5G
+#uci set wireless.default_radio0.encryption=psk2+ccmp
+#uci set wireless.default_radio1.encryption=psk2+ccmp
+#uci set wireless.default_radio0.key=password
+#uci set wireless.default_radio1.key=password
+uci commit wireless
+
+
+#uci commit dhcp
+uci commit network
+
+uci commit
+/etc/init.d/network restart
+#/etc/init.d/odhcpd restart
+
+exit 0
